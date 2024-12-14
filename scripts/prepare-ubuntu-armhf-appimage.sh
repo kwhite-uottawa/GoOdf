f899fa05ea46c1a9afed9e855d9ea062858f8bc7
Keith White <kwhite@uottawa.ca>
2024-12-14 17:18:16 -0500

    Flesh out the commands to use to build an armhf AppImage.

--- scripts/prepare-ubuntu-armhf-appimage.sh
+++ scripts/prepare-ubuntu-armhf-appimage.sh
@@ -12,7 +12,7 @@ sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential cmake \
   libwxgtk3.0-gtk3-dev imagemagick \
   pkgconf libfuse2 \
-  git wget
+  git wget yaru-theme-icon
 
 # linuxdeploy, linuxdeploy-plugin-gtk and appimagetool must be downloaded
 sudo wget -P /usr/local/bin https://raw.githubusercontent.com/linuxdeploy/linuxdeploy-plugin-gtk/master/linuxdeploy-plugin-gtk.sh
@@ -26,6 +26,20 @@ sudo chmod +x /usr/local/bin/appimagetool-armhf.AppImage
 
 exit 0
 
+=== cmake command line? ===
+
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
+make -j2 install DESTDIR=AppDir VERBOSE=1
+DEPLOY_GTK_VERSION=3 APPIMAGE_EXTRACT_AND_RUN=1 \
+	linuxdeploy-armhf.AppImage --appdir AppDir -e ./AppDir/usr/bin/GoOdf \
+	-d ./AppDir/usr/share/GoOdf/applications/GoOdf.desktop -i ./AppDir/usr/share/icons/hicolor/256x256/apps/GoOdf.png \
+	--plugin gtk
+DEPLOY_GTK_VERSION=3 APPIMAGE_EXTRACT_AND_RUN=1 \
+        appimagetool-armhf.AppImage --no-appstream AppDir \
+	GoOdf-kwhite-`git log --oneline | head -1 | awk '{print $1}'`.armhf.AppImage
+
+
+
 === for release.yml? ===
 
     - name: Create Appimage
@@ -33,7 +47,7 @@ exit 0
       run: |
         echo "DEPLOY_GTK_VERSION=3" >> $GITHUB_ENV
         echo "APPIMAGE_EXTRACT_AND_RUN=1" >> $GITHUB_ENV
-        linuxdeploy-armhf.AppImage --appdir AppDir -e ./bin/GoOdf -d ./share/GoOdf/applications/GoOdf.desktop -i ./share/icons/hicolor/256x256/apps/GoOdf.png --plugin gtk
+        linuxdeploy-armhf.AppImage --appdir AppDir -e ./AppDir/usr/bin/GoOdf -d ./AppDir/usr/share/GoOdf/applications/GoOdf.desktop -i ./AppDir/usr/share/icons/hicolor/256x256/apps/GoOdf.png --plugin gtk
         appimagetool-armhf.AppImage --no-appstream AppDir GoOdf-${{env.app_version}}.armhf.AppImage
 
 ===
